ab9128cd4bbd37770c1e8cc20a2ecd23bd982daa
ccama <atil.menes.xd@gmal.com>
2022-02-22 16:39:43 -0500

    cepreuna 3

--- resources/views/livewire/title-component.blade.php
+++ resources/views/livewire/title-component.blade.php
@@ -1,1 +1,1 @@
-<title>{{ ucwords(str_replace('-', ' ', $title)) }} | {{ config('app.name', 'CEPRE-UNAP') }}</title>
+<title>{{ ucwords(str_replace('-', ' ', $title == 'Inicio' ? '': $title.' | ')) }} {{ config('app.name', 'CEPREUNA') }}</title>
